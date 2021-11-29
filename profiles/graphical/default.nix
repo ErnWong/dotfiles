ce4457e9cbf267df98c0cfadedd14a8af7e48464
Ernest Wong <sudo@ernestwong.nz>
2021-11-29 22:23:32 +1300

    Revert "Isolate stack overflow - 3 - disable plasma"
    This reverts commit 0db5b2d722cb5b0843f75d99e1d8ece88a35755c.

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -4,9 +4,9 @@
     opengl.enable = true;
   };
 
-  # services.xserver = {
-  #   enable = true;
-  #   displayManager.sddm.enable = true;
-  #   desktopManager.plasma5.enable = true;
-  # };
+  services.xserver = {
+    enable = true;
+    displayManager.sddm.enable = true;
+    desktopManager.plasma5.enable = true;
+  };
 }
