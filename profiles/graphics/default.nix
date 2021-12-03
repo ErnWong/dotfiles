9506adc4514d788a86ff55c7af213d3da863ac84
Ernest Wong <sudo@ernestwong.nz>
2021-12-03 19:56:45 +1300

    Fix typo - fonts uses arrays with square brackets

--- profiles/graphics/default.nix
+++ profiles/graphics/default.nix
@@ -11,12 +11,12 @@
     desktopManager.plasma5.enable = true;
   };
 
-  fonts.fonts = with pkgs; {
+  fonts.fonts = with pkgs; [
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
     liberation_ttf
     fira-code
     fira-code-symbols
-  };
+  ];
 }
