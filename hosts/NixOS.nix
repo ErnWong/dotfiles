577b2fd9fa6a3101d9a59ece0eed405cea0a3fd1
Ernest Wong <sudo@ernestwong.nz>
2021-12-05 18:27:59 +1300

    Update host with autogenerated hardware configuration and grub config for bios

--- hosts/NixOS.nix
+++ hosts/NixOS.nix
@@ -3,10 +3,29 @@
   ### root password is empty by default ###
   imports = suites.graphical;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/disk/by-label/nixos";
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+    "sr_mod"
+    "rtsx_pci_sdmmc"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [
+    "kvm-intel"
+  ];
+  boot.extraModulePackages = [ ];
 
   networking.networkmanager.enable = true;
+  networking.wireless.enable = true;
+  networking.interfaces.enp4s0f1.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
 
   fileSystems."/" = { device = "/dev/disk/by-label/nixos"; };
+  swapDevices."/" = { device = "/dev/disk/by-label/swap"; };
 }
