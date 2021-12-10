96c499f2b3b702e787d9aa848bf613239b0c1e2e
Ernest Wong <sudo@ernestwong.nz>
2021-12-10 21:34:49 +0000

    Typo fix: Group the platform check correctly

--- users/profiles/comms/default.nix
+++ users/profiles/comms/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    (lib.mkIf stdenv.hostPlatform.system == "x86_64-linux" discord)
+    (lib.mkIf (stdenv.hostPlatform.system == "x86_64-linux") discord)
   ];
   programs = {
     chromium.enable = true;
