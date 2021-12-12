c57f17db8eb6d083efc9267a20eec13a90fc5629
Ernest Wong <sudo@ernestwong.nz>
2021-12-12 00:45:09 +0000

    Add user-local neovim with plugins

--- users/profiles/dev/default.nix
+++ users/profiles/dev/default.nix
@@ -4,6 +4,7 @@
     packages = with pkgs; [
       (lib.mkIf stdenv.hostPlatform.isLinux wezterm)
       gdb
+      rnix-lsp
     ];
     enableDebugInfo = true;
   };
@@ -23,6 +24,91 @@
         version = "0.9.0";
         sha256 = "sha256-6Vcz4EPirN+d4VjzMky7BSHvqMdkf6GqwPiHuPNyaIU=";
       }];
+    };
+    neovim = {
+      enable = true;
+      coc = {
+        enable = true;
+        settings = {
+          languageServer = {
+            nix = {
+              command = "rnix-lsp";
+              filetypes = [ "nix" ];
+            };
+          };
+        };
+      };
+      plugins = with pkgs.vimPlugins; [
+        vim-sensible
+        neovim-sensible
+
+        # Intelligence
+        syntastic
+        editorconfig-vim
+
+        # Languages
+        vim-liquid
+        vim-ps1
+        vim-cpp-enhanced-highlight
+        vim-pandoc
+        vim-pandoc-syntax
+        markdown-preview-nvim
+        #yajs-vim
+        #vim-sass-lint
+        rust-vim
+        yats-vim
+        ats-vim
+        vim-nix
+
+        # Coc
+        #coc-marketplace
+        #coc-xml
+        #coc-json
+        #coc-yaml
+        #coc-toml
+        #coc-gitignore
+        coc-rust-analyzer
+        #coc-java
+        #coc-tsserver
+        #coc-eslint
+        #coc-vimlsp
+        #coc-pyright
+        #coc-powershell
+        #coc-sh
+        #coc-sql
+        #coc-docker
+        #coc-vimtex
+        #coc-css
+        #coc-prettier
+
+        # Handy
+        vim-surround
+        vim-commentary
+        #fzf
+        fzf-vim
+
+        # UI
+        vim-airline
+        vim-airline-themes
+        vim-tmux-navigator
+        vim-bufkill
+        ranger-vim
+        tagbar
+
+        # Git
+        vim-fugitive
+        vim-rhubarb
+        vim-gitgutter
+
+        # Colorschemes
+        gruvbox
+      ];
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      withNodeJS = true;
+      withPython3 = true;
+      withRuby = true;
     };
   };
 }
