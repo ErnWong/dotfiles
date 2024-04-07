63fde00a2d4f9603c897f4bac9917983ec7c89a3
Ernest Wong <sudo@ernestwong.nz>
2024-04-07 18:18:21 +1200

    Attach swap drive

--- hosts/kaiki.nix
+++ hosts/kaiki.nix
@@ -17,7 +17,9 @@
       fsType = "btrfs";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/dev/disk/by-label/swap"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
