25c0a29ba53d5328a002e4d73691593cad13b1b3
Ernest Wong <sudo@ernestwong.nz>
2024-04-14 22:37:55 +1200

    Use buildGoModule/mkYarnPackage - now builds
    Based on https://github.com/NixOS/nixpkgs/blob/01c1cf524dd9f46f8d05f4e296d48157f16f6671/pkgs/applications/misc/kuro/default.nix#L8
    and https://github.com/NixOS/nixpkgs/blob/c2529df52aff216f8681faeb6be2ade6239b2672/pkgs/servers/etcd/3.4.nix#L4

--- pkgs/waveterm/default.nix
+++ pkgs/waveterm/default.nix
@@ -1,4 +1,15 @@
-{ pkgs, stdenv, fetchFromGitHub }:
+{
+  lib,
+  fetchFromGitHub,
+  fetchYarnDeps,
+  buildGoModule,
+  mkYarnPackage,
+  electron_29,
+  makeWrapper,
+  copyDesktopItems,
+  makeDesktopItem,
+  breakpointHook
+}:
 let
   pname = "waveterm";
   version = "0.7.2";
@@ -8,52 +19,116 @@ let
     rev = "v${version}";
     sha256 = "sha256-NxUcBiwCK5ViAQVpx1P3pwvsr69p/ai/KztwZOFUt80=";
   };
-  yarnOfflineCache = pkgs.fetchYarnDeps {
-    yarnLock = "${src}/yarn.lock";
-    hash = "sha256-LGh2KmqwBfpf6MC77ME9dx43Gg3RxTpd+KSBIkEbfT0=";
+  wavesrv = buildGoModule {
+    inherit version src;
+    preBuild = ''
+      # go: 'go mod vendor' cannot be run in workspace mode. Run 'go work vendor' to vendor the workspace or set 'GOWORK=off' to exit workspace mode.
+      # https://github.com/NixOS/nixpkgs/issues/203039
+      export GOWORK="off";
+    '';
+    pname = "${pname}-wavesrv";
+    modRoot = "./wavesrv";
+    vendorHash = "sha256-1pUQ1yrdzsotvcX6ydomhlwFwl6TZTXAn/oL8a1p9MY=";
+    doCheck = false; # Currently fails at comp_test.go:91: comp-fail: ls f[*] + [foo bar] => [ls foo\ bar [*]] expected[ls 'foo bar' [*]]
   };
+  waveshell = buildGoModule {
+    inherit version src;
+    preBuild = ''
+      # go: 'go mod vendor' cannot be run in workspace mode. Run 'go work vendor' to vendor the workspace or set 'GOWORK=off' to exit workspace mode.
+      # https://github.com/NixOS/nixpkgs/issues/203039
+      export GOWORK="off";
+    '';
+    pname = "${pname}-waveshell";
+    modRoot = "./waveshell";
+    vendorHash = "sha256-bkFv4hi4q36mJ9QUsKz00DdUZkRdx/JLJRutPcx+RLQ=";
+  };
+  electron = electron_29;
 in
-  stdenv.mkDerivation {
-    inherit pname version src;
-    name = "Wave Terminal";
+  mkYarnPackage rec {
+    inherit pname src version;
+
+    packageJSON = "${src}/package.json";
+
+    env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+
+    offlineCache = fetchYarnDeps {
+      yarnLock = "${src}/yarn.lock";
+      hash = "sha256-LGh2KmqwBfpf6MC77ME9dx43Gg3RxTpd+KSBIkEbfT0=";
+    };
 
     nativeBuildInputs = [
-      pkgs.go
-      pkgs.scripthaus
-      pkgs.nodejs_21
-      pkgs.yarn
-      pkgs.fixup_yarn_lock
-
-      pkgs.breakpointHook
+      makeWrapper
+      copyDesktopItems
+      breakpointHook
     ];
-    buildInputs = [
-
-    ];
-
-    configurePhase = ''
-      # Yarn wants to write to ~/.yarnrc
-      export HOME=$(mktemp -d)
-    '';
 
     buildPhase = ''
-      echo ${yarnOfflineCache}
-      yarn config --offline set yarn-offline-mirror ${yarnOfflineCache}
-      fixup_yarn_lock yarn.lock
-      yarn install --offline --frozen-lockfile --ignore-platform --ignore-scripts --no-progress --no-interactive
-
-      # Ask scripthaus to not write history to read-only home folder.
-      mkdir ./scripthaus
-      export SCRIPTHAUS_HOME=$(pwd)/scripthaus
-      touch ./scripthaus/.nohistory
-
-      #mkdir ./bin
-      #ls .
-
-      echo $out
-
-      #scripthaus run electron-rebuild
-      #scripthaus run build-backend
-      #scripthaus run webpack-build-prod
-      scripthaus run build-package-linux
+      pushd deps/${pname}
+      node_modules/.bin/webpack --env prod
+      popd
     '';
+    
+    postBuild = ''
+      pushd deps/${pname}
+
+      ls ${wavesrv}/bin
+      mkdir ./bin
+      ls ${waveshell}/bin
+      cp -r ${wavesrv}/bin/. ./bin/.
+      cp -r ${waveshell}/bin/. ./bin/.
+      ls bin
+
+      yarn --offline run electron-builder \
+        -l \
+        -p never \
+        -c electron-builder.config.js \
+        -c.electronDist=${electron}/libexec/electron \
+        -c.electronVersion=${electron.version}
+
+      popd
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      ## resources
+      #mkdir -p "$out/share/lib/${pname}"
+      #cp -r ./deps/${pname}/dist/*-unpacked/{locales,resources{,.pak}} "$out/share/lib/${pname}"
+
+      ## icons
+      #install -Dm644 ./deps/${pname}/static/Icon.png $out/share/icons/hicolor/1024x1024/apps/${pname}.png
+
+      ## executable wrapper
+      #makeWrapper '${electron}/bin/electron' "$out/bin/${pname}" \
+      #  --add-flags "$out/share/lib/${pname}/resources/app.asar" \
+      #  --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}" \
+      #  --inherit-argv0
+
+      runHook postInstall
+    '';
+    # Do not attempt generating a tarball for contents again.
+    # note: `doDist = false;` does not work.
+    distPhase = "true";
+
+    desktopItems = [
+      (makeDesktopItem {
+        name = pname;
+        exec = pname;
+        icon = pname;
+        desktopName = "Wave Terminal";
+        genericName = "An Open-Source, AI-Native, Terminal Built for Seamless Workflows";
+        comment = meta.description;
+        categories = [ "System" ];
+        startupWMClass = pname;
+      })
+    ];
+
+    meta = with lib; {
+      changelog = "https://github.com/wavetermdev/waveterm/releases/tag/${src.rev}";
+      description = "An unofficial, featureful, open source, community-driven, free Microsoft To-Do app";
+      homepage = "https://github.com/wavetermdev/wavetermn";
+      license = licenses.asl20;
+      mainProgram = pname;
+      inherit (electron.meta) platforms;
+    };
   }
