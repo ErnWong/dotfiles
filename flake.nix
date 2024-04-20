3c426b16580fc037f3595b5a2d393f9a4638ef46
Ernest Wong <sudo@ernestwong.nz>
2024-04-20 15:58:57 +1200

    Try adding linters and formatters to checks

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "Ernest's nix configuration";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -16,27 +17,78 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, musnix, nixos-cosmic, ... }@inputs: {
-    packages.x86_64-linux = import ./pkgs nixpkgs.legacyPackages.x86_64-linux;
-    nixosConfigurations = {
-      yoroizuka = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          musnix.nixosModules.default
-          nixos-cosmic.nixosModules.default
-          ./configuration.nix
-          ./hosts/yoroizuka.nix
-        ];
+
+  outputs = inputs:
+    let
+      pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    in rec {
+      formatter.x86_64-linux = pkgs.nixfmt;
+
+      checks.x86_64-linux = packages.x86_64-linux // {
+        format = pkgs.stdenvNoCC.mkDerivation {
+          name = "format";
+          dontBuild = true;
+          doCheck = true;
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.nixfmt-rfc-style
+          ];
+          checkPhase = ''
+            nixfmt check
+            touch "$out"
+          '';
+        };
+
+        lint-statix = pkgs.stdenvNoCC.mkDerivation {
+          name = "lint-statix";
+          dontBuild = true;
+          doCheck = true;
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.statix
+          ];
+          checkPhase = ''
+            statix check .
+            touch "$out"
+          '';
+        };
+
+        lint-deadnix = pkgs.stdenvNoCC.mkDerivation {
+          name = "lint-deadnix";
+          dontBuild = true;
+          doCheck = true;
+          src = ./.;
+          nativeBuildInputs = [
+            pkgs.deadnix
+          ];
+          checkPhase = ''
+            deadnix --fail
+            touch "$out"
+          '';
+        };
       };
-      kaiki = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [
-          musnix.nixosModules.default
-          nixos-cosmic.nixosModules.default
-          ./configuration.nix
-          ./hosts/kaiki.nix
-        ];
+
+      packages.x86_64-linux = import ./pkgs pkgs;
+
+      nixosConfigurations = {
+        yoroizuka = inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            inputs.musnix.nixosModules.default
+            inputs.nixos-cosmic.nixosModules.default
+            ./configuration.nix
+            ./hosts/yoroizuka.nix
+          ];
+        };
+        kaiki = inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            inputs.musnix.nixosModules.default
+            inputs.nixos-cosmic.nixosModules.default
+            ./configuration.nix
+            ./hosts/kaiki.nix
+          ];
+        };
       };
-    };
   };
 }
