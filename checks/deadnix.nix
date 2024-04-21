ec040d913810eeee22cfa23e0ae1775753aae326
Ernest Wong <sudo@ernestwong.nz>
2024-04-21 20:44:43 +1200

    Add annotation generators for deadnix and statix

--- checks/deadnix.nix
+++ checks/deadnix.nix
@@ -4,5 +4,14 @@ pkgs:
   packages = [ pkgs.deadnix ];
   machine-readable = "deadnix --fail --output-format json";
   human-readable = "deadnix --fail";
-  to-github-annotations = "echo $in";
+  to-github-annotations = ''
+    from json --objects
+    | each {|file|
+      $file.results | each {|result|
+          $"::warning file=($file.file),line=($result.line),endline=($result.line)::($result.message)"
+      }
+      | str join "\n"
+    }
+    | str join "\n"
+  '';
 }
--- checks/statix.nix
+++ checks/statix.nix
@@ -3,5 +3,27 @@ pkgs:
   packages = [ pkgs.statix ];
   machine-readable = "statix check . --format json";
   human-readable = "statix check .";
-  to-github-annotations = "echo $in";
+  to-github-annotations = ''
+    split row --regex "\n}"
+    | drop 1
+    | each {|file_string|
+      let file = $"($file_string)}"
+      | from json
+
+      $file | $in.report
+      | each {|report|
+        let level = match $report.severity {
+          'Hint' => 'notice'
+          'Warn' => 'warning'
+          'Error' => 'error'
+        }
+        $report.diagnostics | each {|diagnostic|
+          $"::($level) file=($file.file),line=($diagnostic.at.from.line),endline=($diagnostic.at.to.line),title=($report.note)::($diagnostic.message)"
+        }
+        | str join "\n"
+      }
+      | str join "\n"
+    }
+    | str join "\n"
+  '';
 }
