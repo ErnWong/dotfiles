41afd9f23c9573f0a3efeb362ead9d9c1b0fef01
Ernest Wong <sudo@ernestwong.nz>
2025-10-27 20:21:09 +1300

    Re-enable garnix cache - since they've said they've improved it now

--- configuration.nix
+++ configuration.nix
@@ -134,13 +134,13 @@
         "https://nix-community.cachix.org"
         "https://cosmic.cachix.org"
         "https://ernwong.cachix.org"
-        #"https://cache.garnix.io"
+        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "ernwong.cachix.org-1:wCKqhqe6/Wxq70Gft+qV2Xh/qfufrvfELgSnkpi58yA="
-        #"cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
 
       experimental-features = [
