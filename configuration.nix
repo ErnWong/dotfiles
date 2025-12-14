721b071355d7b1dde9eb104b723be62c97addf13
Ernest Wong <sudo@ernestwong.nz>
2025-12-14 23:21:27 +1300

    Update ubuntu font name

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,7 @@
       pkgs.noto-fonts-cjk-sans
       pkgs.noto-fonts-color-emoji
       pkgs.liberation_ttf
-      pkgs.ubuntu_font_family
+      pkgs.ubuntu-classic
       pkgs.fira-code
       pkgs.fira-code-symbols
       pkgs.roboto
