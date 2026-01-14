4d91a0d1636b1ad5884c2d1b3181001c54e49b08
Ernest Wong <sudo@ernestwong.nz>
2026-01-15 00:13:33 +1300

    Share wifi internet with ethernet for kaiki

--- hosts/kaiki.nix
+++ hosts/kaiki.nix
@@ -55,10 +55,66 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
+  #networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   networking.hostName = "kaiki";
+
+  # Internet Connection Sharing - share the wifi internet with ethernet.
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  networking.interfaces."eno1" = {
+    useDHCP = false;
+    ipv4.addresses = [{
+      address = "10.0.0.1";
+      prefixLength = 24;
+    }];
+  };
+  networking.nftables.ruleset = ''
+    table ip nat {
+      chain POSTROUTING {
+        type nat hook postrouting priority 100;
+        oifname "wlp11s0" counter masquerade
+      }
+    }
+    table ip filter {
+      chain INPUT {
+        iifname "eno1" counter accept
+      }
+    }
+  '';
+  services.kea.dhcp4 = {
+    enable = true;
+    settings = {
+      interfaces-config = {
+        interfaces = [
+          "eno1"
+        ];
+      };
+      lease-database = {
+        name = "/var/lib/kea/dhcp4.leases";
+        persist = true;
+        type = "memfile";
+      };
+      rebind-timer = 2000;
+      renew-timer = 1000;
+      subnet4 = [
+        {
+          id = 1;
+          pools = [
+            {
+              pool = "10.0.0.2 - 10.0.0.255";
+            }
+          ];
+          subnet = "10.0.0.1/24";
+        }
+      ];
+      valid-lifetime = 4000;
+      option-data = [{
+        name = "routers";
+        data = "10.0.0.1";
+      }];
+    };
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
