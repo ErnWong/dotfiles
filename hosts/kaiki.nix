b192b4577678311ab92143d78c8a70c05b8ee2b0
Ernest Wong <sudo@ernestwong.nz>
2026-02-08 20:23:30 +1300

    Try (fail) share ipv6 via nat66 due to wifi restrictions

--- hosts/kaiki.nix
+++ hosts/kaiki.nix
@@ -61,60 +61,137 @@
   networking.hostName = "kaiki";
 
   # Internet Connection Sharing - share the wifi internet with ethernet.
-  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
-  networking.interfaces."eno1" = {
-    useDHCP = false;
-    ipv4.addresses = [{
-      address = "10.0.0.1";
-      prefixLength = 24;
-    }];
-  };
-  networking.nftables.ruleset = ''
-    table ip nat {
-      chain POSTROUTING {
-        type nat hook postrouting priority 100;
-        oifname "wlp11s0" counter masquerade
-      }
-    }
-    table ip filter {
-      chain INPUT {
-        iifname "eno1" counter accept
-      }
-    }
-  '';
-  services.kea.dhcp4 = {
-    enable = true;
-    settings = {
-      interfaces-config = {
-        interfaces = [
-          "eno1"
-        ];
-      };
-      lease-database = {
-        name = "/var/lib/kea/dhcp4.leases";
-        persist = true;
-        type = "memfile";
-      };
-      rebind-timer = 2000;
-      renew-timer = 1000;
-      subnet4 = [
-        {
-          id = 1;
-          pools = [
-            {
-              pool = "10.0.0.2 - 10.0.0.255";
-            }
-          ];
-          subnet = "10.0.0.1/24";
-        }
-      ];
-      valid-lifetime = 4000;
-      option-data = [{
-        name = "routers";
-        data = "10.0.0.1";
-      }];
+  networking.networkmanager.ensureProfiles.profiles = {
+    "Shared" = {
+      connection.type = "ethernet";
+      connection.id = "Shared Ethernet";
+      connection.interface-name = "eno1";
+
+      ipv4.method = "shared";
+      ipv4.addresses = "10.0.0.1/24";
+      ipv6.method = "shared";
+      ipv6.addresses = "fd00:5cff:fe1c:d4bc::1/64";
     };
   };
+  networking.firewall.allowedUDPPorts = [53 67];
+  networking.nftables.tables.myipv6nat = {
+    family = "ip6";
+    # Copied from what networkmanager is doing for ipv4
+    content = ''
+	chain nat_postrouting {
+		type nat hook postrouting priority srcnat; policy accept;
+		ip6 saddr fd00:5cff:fe1c:d4bc::1/64 ip6 daddr != fd00:5cff:fe1c:d4bc::1/64 masquerade
+	}
+
+	chain filter_forward {
+		type filter hook forward priority filter; policy accept;
+		ip6 daddr fd00:5cff:fe1c:d4bc::1/64 oifname "eno1" ct state { established, related } accept
+		ip6 saddr fd00:5cff:fe1c:d4bc::1/64 iifname "eno1" accept
+		iifname "eno1" oifname "eno1" accept
+		iifname "eno1" reject
+		oifname "eno1" reject
+	}
+
+    '';
+  };
+  #networking.nat = {
+  #  enable = true;
+  #  enableIPv6 = true;
+  #  #internalInterfaces = ["eno1"];
+  #  internalIPv6s = [ "fd00:5cff:fe1c:d4bc:428d:5cff:fe1c:d4bc"];
+  #  externalInterface = "wlp11s0";
+  #};
+  # services.kea.dhcp6 = {
+  #   enable = true; # We only have /64 prefix from ISP so can't delegate further prefixes and use SLAAC, so start our own DHCPv6 server instead
+  #   settings = {
+  #     interfaces-config = {
+  #       interfaces = [
+  #         "eno1"
+  #       ];
+  #     };
+  #     lease-database = {
+  #       name = "/var/lib/kea/dhcp6.leases";
+  #       persist = true;
+  #       type = "memfile";
+  #     };
+  #     rebind-timer = 2000;
+  #     renew-timer = 1000;
+  #     subnet6 = [
+  #       {
+  #         id = 1;
+  #         pools = [
+  #           {
+  #             pool = ?????????? 
+  #           }
+  #         ];
+  #         subnet = ????????????
+  #       }
+  #     ];
+  #     valid-lifetime = 4000;
+  #     # option-data = [{
+  #     #   name = "routers";
+  #     #   data = "10.0.0.1";
+  #     # }];
+  #   };
+  # };
+  
+  #networking.useDHCP = lib.mkForce false;
+  #boot.kernel.sysctl."net.ipv4.ip_forward" = true;
+  #boot.kernel.sysctl."net.ipv6.ip_forward" = true;
+  #networking.interfaces."wlp11s0".useDHCP = true;
+  #networking.interfaces."eno1" = {
+  #  useDHCP = lib.mkForce false;
+  #  ipv4.addresses = lib.mkForce [{
+  #    address = "10.0.0.1";
+  #    prefixLength = 24;
+  #  }];
+  #};
+  #networking.nftables.ruleset = ''
+  #  table ip nat {
+  #    chain POSTROUTING {
+  #      type nat hook postrouting priority 100;
+  #      oifname "wlp11s0" counter masquerade
+  #    }
+  #  }
+  #  table ip filter {
+  #    chain INPUT {
+  #      iifname "eno1" counter accept
+  #    }
+  #  }
+  #'';
+  #services.kea.dhcp4 = {
+  #  enable = true;
+  #  settings = {
+  #    interfaces-config = {
+  #      interfaces = [
+  #        "eno1"
+  #      ];
+  #    };
+  #    lease-database = {
+  #      name = "/var/lib/kea/dhcp4.leases";
+  #      persist = true;
+  #      type = "memfile";
+  #    };
+  #    rebind-timer = 2000;
+  #    renew-timer = 1000;
+  #    subnet4 = [
+  #      {
+  #        id = 1;
+  #        pools = [
+  #          {
+  #            pool = "10.0.0.2 - 10.0.0.255";
+  #          }
+  #        ];
+  #        subnet = "10.0.0.1/24";
+  #      }
+  #    ];
+  #    valid-lifetime = 4000;
+  #    option-data = [{
+  #      name = "routers";
+  #      data = "10.0.0.1";
+  #    }];
+  #  };
+  #};
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
